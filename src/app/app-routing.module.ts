34b22b7dbea8934ca88170e1099d1ff895e55b6d
aim4dabush <edarasato@gmail.com>
2023-01-04 00:27:28 -0800

    setup components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//components
+import { CountryDetailsComponent } from './country-details/country-details.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: ':country', component: CountryDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,13 @@ import { NgModule } from '@angular/core';
 //components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './home/search/search.component';
+import { RegionComponent } from './home/region/region.component';
+import { CountryDetailsComponent } from './country-details/country-details.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, RegionComponent, CountryDetailsComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
